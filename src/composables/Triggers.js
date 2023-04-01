febe19ff770fe14a794d6418d4f319bae04790b2
yhuhz <julius.albert.grajo@gmail.com>
2023-04-01 14:41:45 +0800

    Removed all for merging

--- src/composables/Triggers.js
+++
@@ -1,9 +0,0 @@
-import { ref, readonly } from 'vue'
-const Dialog = ref(false);
-let DialogState = readonly(Dialog)
-
-const ToggleDialogState = () => {
-  Dialog.value = !Dialog.value
-}
-
-export { DialogState, ToggleDialogState }
